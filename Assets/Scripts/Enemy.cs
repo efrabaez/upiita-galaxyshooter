50a99a2fe13548ac7f7c56b7dff13c086d4ca750
Efraín Germán Báez Rubio <efrain@hyperlogics.com>
2020-04-21 11:20:30 -0500

    Enemy prefab && behaviour

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] //Serialize Data in order to read it and modified form inspector, but not from other game objects.
+    private float _speed = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveDown();
+    }
+
+    void MoveDown()
+    {
+        transform.Translate(translation: Vector3.down * _speed * Time.deltaTime);
+
+        if (transform.position.y < -6.0f)
+        {
+            transform.position = new Vector3(Random.Range(-9.5f, 9.5f), 7, 0);
+        }
+    }
+}
