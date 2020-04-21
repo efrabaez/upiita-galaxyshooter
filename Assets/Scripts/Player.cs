345a37ce79767977843d9d46a613a736ad81dee5
Efraín Germán Báez Rubio <efrain@hyperlogics.com>
2020-04-20 19:49:56 -0500

    Player horizontal and vertical movement with user input

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] //Serialize Data in order to read it and modified form inspector, but not from other game objects.
+    private float _speed = 5.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +17,13 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal"); //Get input from keyboard arrows or a|d
+        float verticalInput = Input.GetAxis("Vertical"); //Get input from keyboard arrows or w|s
+        //First way
+        //transform.Translate(translation: Vector3.right * horizontalInput * _speed * Time.deltaTime);
+        //transform.Translate(translation: Vector3.up * verticalInput * _speed * Time.deltaTime);
+
+        //Better way
+        transform.Translate(translation: new Vector3(horizontalInput, verticalInput, 0) * _speed * Time.deltaTime);
     }
 }
