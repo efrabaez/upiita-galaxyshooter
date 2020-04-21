f18f6d61d42ef905fc52685aada3127eaca1bfec
Efraín Germán Báez Rubio <efrain@hyperlogics.com>
2020-04-20 20:21:59 -0500

    Separete in functions and player bounds

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,13 +17,32 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CalculateMovement();
+
+    }
+
+    void CalculateMovement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal"); //Get input from keyboard arrows or a|d
         float verticalInput = Input.GetAxis("Vertical"); //Get input from keyboard arrows or w|s
-        //First way
-        //transform.Translate(translation: Vector3.right * horizontalInput * _speed * Time.deltaTime);
-        //transform.Translate(translation: Vector3.up * verticalInput * _speed * Time.deltaTime);
+        /*First way
+         * transform.Translate(translation: Vector3.right * horizontalInput * _speed * Time.deltaTime);
+         * transform.Translate(translation: Vector3.up * verticalInput * _speed * Time.deltaTime);
+         */
 
         //Better way
         transform.Translate(translation: new Vector3(horizontalInput, verticalInput, 0) * _speed * Time.deltaTime);
+
+
+        /*
+         * Player bounds
+         */
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0),0);
+
+        if (transform.position.x > 12 || transform.position.x < -12)
+        {
+            transform.position = new Vector3(-transform.position.x, transform.position.y, 0);
+        }
     }
 }
